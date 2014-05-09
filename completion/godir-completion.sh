b498d7a366aa3cd883f91bb90fd5c5d2550e56a0
Kent Chen <chenkaie@gmail.com>
2014-05-09 17:34:14 +0800

    Add godir-complete.sh by Oswin, it's really AWESOME for godir()

---
+++ completion/godir-completion.sh
@@ -0,0 +1,36 @@
+# file: godir.sh
+# godir completion
+
+_test_filenames()
+{
+	for f in "${COMPREPLY[@]}"; do
+		if [ -n "`echo \"$f\" | grep /$`" ]; then
+			return 1
+		fi
+	done 
+	return 0
+}
+
+_godir()
+{
+	[ -z "$PRODUCTDIR" ] && return 0
+	[ ! -f "$PRODUCTDIR/.filelist" ] && return 0
+
+	local cur
+	COMPREPLY=()
+	cur=${COMP_WORDS[COMP_CWORD]}
+
+	COMPREPLY=($(grep "/$cur[^/]*/\?$" "$PRODUCTDIR/.filelist" | perl -pe "s,.*?/$cur,$cur,"))
+
+	_test_filenames
+	if [ $? -eq 0 ]; then
+		# If a file name is completed, let the completion trail a space
+		compopt -o filenames
+	else
+		compopt -o nospace
+	fi
+
+	return 0
+}
+
+complete -F _godir godir
