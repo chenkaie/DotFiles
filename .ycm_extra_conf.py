693c072716e9333714a17d6b8a568ffbdf34b57f
Kent Chen <kent.chen@ubnt.com>
2015-05-05 01:20:08 +0800

    Dynamic C/C++ detection with filename extension to add correct flags

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -46,21 +46,7 @@ flags = [
 # You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
 # source code needs it.
 '-DUSE_CLANG_COMPLETER',
-# THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
-# language to use when compiling headers. So it will guess. Badly. So C++
-# headers will be compiled as C headers. You don't want that so ALWAYS specify
-# a "-std=<something>".
-# For a C project, you would set this to something like 'c99' instead of
-# 'c++11'.
-#'-std=c++11',
-'-std=gnu99',
-# ...and the same thing goes for the magic -x option which specifies the
-# language that the files to be compiled are written in. This is mostly
-# relevant for c++ headers.
-# For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
-#'c++',
-'c',
+#Kent separate into flags_c[] & flags_cpp[]
 #Kent added begin (Ubuntu 14.10 -> $CC -v -E -x c /dev/null 2>&1)
 '-I',
 '.',
@@ -75,6 +61,30 @@ flags = [
 '-isystem',
 '/usr/include',
 #Kent added End
+]
+
+
+flags_c = [
+# THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
+# language to use when compiling headers. So it will guess. Badly. So C++
+# headers will be compiled as C headers. You don't want that so ALWAYS specify
+# a "-std=<something>".
+# For a C project, you would set this to something like 'c99' instead of 'c++11'.
+#'-std=c++11',
+'-std=gnu99',
+# ...and the same thing goes for the magic -x option which specifies the
+# language that the files to be compiled are written in. This is mostly
+# relevant for c++ headers.
+# For a C project, you would set this to 'c' instead of 'c++'.
+'-x',
+#'c++',
+'c',
+]
+
+flags_cpp = [
+'-std=c++11',
+'-x',
+'c++'
 ]
 
 
@@ -174,7 +184,15 @@ def FlagsForFile( filename, **kwargs ):
       pass
   else:
     relative_to = DirectoryOfThisScript()
-    final_flags = MakeRelativePathsInFlagsAbsolute( flags, relative_to )
+
+    if filename.endswith(".c") or filename.endswith(".h"):
+        full_flags = flags + flags_c
+    elif filename.endswith(".cpp") or filename.endswith(".cc") or filename.endswith(".hpp"):
+        full_flags = flags + flags_cpp
+    else:
+        full_flags = flags + flags_c
+
+    final_flags = MakeRelativePathsInFlagsAbsolute( full_flags, relative_to )
 
   return {
     'flags': final_flags,
